15031500070c7ffeccb7a97d75fe96933b18ec74
JoeVenton <joe.venton@gmail.com>
2023-06-27 17:01:22 +0100

    added logic and css

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 "use strict"
 
 const pContainer = document.querySelector("section");
-const resultButton = document.querySelector("section + div");
+const resultButton = document.querySelector("section");
 const image1 = document.querySelector("section img:first-child");
 const image2 = document.querySelector("section img:nth-child(2)");
 const image3 = document.querySelector("section img:nth-child(3)");
-
 let clicks = 0;
 const maxClicks = 25;
 
 let pList = [];
+console.log(pList)
 
 function getRandomNumber() {
-  return Math.floor(Math.random() * pList.length);
+    return Math.floor(Math.random() * pList.length);
 }
 
 function Product(name, src) {
@@ -28,12 +28,14 @@ function renderProducts() {
   let p2 = getRandomNumber();
   let p3 = getRandomNumber();
 
-  while (p1 === p2 || p3) {
-    p1 = getRandomNumber();
-  }
-
-  while (p2 === p3) {
+  while (p1 === p2) {
     p2 = getRandomNumber();
+  }
+  while (p1 === p3) {
+    p3 = getRandomNumber();
+  }
+  while (p2 === p3) {
+    p3 = getRandomNumber();
   }
 
   image1.src = pList[p1].src;
@@ -46,13 +48,15 @@ function renderProducts() {
   pList[p2].views++;
   pList[p3].views++;
 }
-
+console.log(image1)
+console.log(image2)
+console.log(image3)
 function handlePClick(event) {
   if (event.target === pContainer) {
     alert("Please click on a product");
   } else {
     clicks++;
-    // console.log(clicks);
+    console.log(clicks);
     let clickedProduct = event.target.alt;
     for (let i = 0; i < pList.length; i++) {
       if (clickedProduct === pList[i].name) {
@@ -68,6 +72,26 @@ function handlePClick(event) {
     renderProducts();
   }
 }
+
+const bag = new Product("bag", "/assets/bag.jpg")
+const banana = new Product("banana", "/assets/banana.jpg")
+const bathroom = new Product("bathroom", "/assets/bathroom.jpg")
+const boots = new Product("boots","/assets/boots.jpg")
+const breakfast = new Product("breakfast", "/assets/breakfast.jpg")
+const bubblegum = new Product("bubblegum","/assets/bubblegum.jpg")
+const chair = new Product("chair","/assets/chair.jpg")
+const cthulhu = new Product("cthulhu", "/assets/cthulhu.jpg")
+const dogduck = new Product ("dog-duck","/assets/dog-duck.jpg")
+const dragon = new Product("dragon","/assets/dragon.jpg")
+const pen = new Product("pen","/assets/pen.jpg")
+const petsweep = new Product("pet-sweep","/assets/pet-sweep.jpg")
+const scissors = new Product("scissors","/assets/scissors.jpg")
+const shark = new Product("shark","/assets/shark.jpg")
+const sweep = new Product("sweep","/assets/sweep.png")
+const tauntaun = new Product("tauntaun","/assets/tauntaun.jpg")
+const unicorn = new Product("unicorn", "/assets/unicorn.jpg")
+const watercan = new Product("water-can","/assets/water-can.jpg")
+const wineglass = new Product("wine-glass","/assets/wine-glass.jpg")
 
 
 
